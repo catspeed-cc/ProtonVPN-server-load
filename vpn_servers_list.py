88ab6c3af37205d2aa97978bb258865f1869258d
mooleshacat <admin@catspeed.cc>
2024-11-25 18:11:55 -0500

    Update vpn_servers_list.py

--- vpn_servers_list.py
+++ vpn_servers_list.py
@@ -2,4 +2,4 @@ import vpn_servers as vpns
 
 # We test by searching for location = "US" servers under 40% load to get all United States servers
 # You could also search for location = "US-CO" for example, to get all Colorado, United States servers
-vpns.find_a_server(location = "US", num_results = 5, max_load = 40)
+vpns.find_a_server(location = "US", num_results = 5, max_load = 95)
