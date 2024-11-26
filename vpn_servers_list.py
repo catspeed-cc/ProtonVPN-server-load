4d78fe8ad5c50baf6afa1666a9cebd3bed2e4c5a
mooleshacat <admin@catspeed.cc>
2024-11-25 19:01:55 -0500

    Update vpn_servers_list.py

--- vpn_servers_list.py
+++ vpn_servers_list.py
@@ -13,8 +13,8 @@ import vpn_servers as vpns
 # You can search for the 2 letter country code with a - at the end, and if you get nothing, try it with a # at the end.
 #
 print("US-CA# servers:")
-vpns.find_a_server(country_code = "US", state_code = "CA", num_results = 5, max_load = 40)
+vpns.find_a_server(country_code = "US", state_code = "CA", max_load = 40, num_results = 5)
 print("US-xx# servers:")
-vpns.find_a_server(country_code = "US", state_code = "", num_results = 5, max_load = 40)
+vpns.find_a_server(country_code = "US", state_code = "", max_load = 40, num_results = 5)
 print("CA# servers:")
-vpns.find_a_server(country_code = "CA", state_code = "", num_results = 5, max_load = 40)
+vpns.find_a_server(country_code = "CA", state_code = "", max_load = 40, num_results = 5)
