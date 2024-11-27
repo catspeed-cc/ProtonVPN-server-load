e2a712f485b24700c6fb82a65d85c56e7ad8745e
mooleshacat <admin@catspeed.cc>
2024-11-27 05:51:05 -0500

    Update vpn_servers_list.py

--- vpn_servers_list.py
+++ vpn_servers_list.py
@@ -5,6 +5,8 @@ import vpn_servers as vpns
 #
 # Note most countries do not have state_codes - or province codes - listed, ex. Canada, Chile, Germany, etc.
 #
+# It is best to search with a state/province code, because if no results are found, it will fall back to search country only
+#
 print("US-CA# servers:")
 vpns.find_a_server(country_code = "US", state_code = "CA", max_load = 40, num_results = 5)
 print("US-xx# servers:")
